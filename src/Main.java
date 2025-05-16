8232a13bef79c37937ac59ccc8b377d2f135a299
admin <krzisio27@gmail.com>
2025-05-16 12:55:07 +0200

    krzysiek commit

--- src/Main.java
+++ src/Main.java
@@ -33,15 +33,46 @@ public class Main
         }
 
         //odczyt plików
-        String path = "./file.txt";
-        BufferedReader reader = new BufferedReader(new FileReader(path));
-        //odczytanie całego pliku linia po linii
-        String line;
-        reader.readLine();
-        while ((line = reader.readLine()) != null) {
-        }
 
+
+            String path = "./file.txt";
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            //odczytanie całego pliku linia po linii
+            String line;
+            reader.readLine();
+            while ((line = reader.readLine()) != null) {
+            }
 
     }
 
+    //pisanie w pliku
+    public void FileWriter()
+    {
+        File file = new File("out.txt");
+        try {
+            PrintWriter writer = new PrintWriter(file);
+            writer.print("First line\n");
+            writer.println("Second line");
+            writer.printf("%s line", "Third\n");
+            writer.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("Cannot open: " + file.getName());
+        }
+    }
+    // StringBuilder
+   public void StringBuilder()
+    {
+        File file = new File("in.txt");
+        StringBuilder result = new StringBuilder();
+        try {
+            FileReader reader = new FileReader(file);
+            int next;
+            while ((next = reader.read()) != -1)
+                result.append((char) next);
+            reader.close();
+        } catch (IOException e) {
+            System.err.println("Cannot access: " + file.getName());
+        }
+        System.out.println(result);
+    }
 }
